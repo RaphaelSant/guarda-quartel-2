52b6762357b4655e66ac0c62e088c5756af3d1ef
Raphael Santiago <raphael.sant.emp@gmail.com>
2024-01-10 09:46:49 -0300

    Método de comunicação do bd alterado

--- Backend/controllers/dbConfig.js
+++ Backend/controllers/dbConfig.js
@@ -1,12 +1,21 @@
 const mysql = require("mysql");
 
+/*
 const db = mysql.createConnection({
+    database: "sisregex",
     host: "localhost",
     user: "root",
     password: "root123",
+});
+*/
+
+const dbConfig = ({
     database: "sisregex",
+    host: "localhost",
+    user: "root",
+    password: "root123"
 });
 
-db.connect();
+const connection = mysql.createPool(dbConfig);
 
-module.exports = db;
+module.exports = connection;
