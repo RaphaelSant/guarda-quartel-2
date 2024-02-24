53a48a18ace1085b503e24ec34119d9606b580d7
Raphael Moura Santiago <raphael.sant.emp@gmail.com>
2024-02-24 11:38:55 -0300

    Página de login atualizada

--- Frontend/src/pages/login/index.jsx
+++ Frontend/src/pages/login/index.jsx
@@ -33,67 +33,79 @@ export default function Login() {
           setMensagem('Usuário não autenticado');
         }
       } else {
-        console.log(mensagem);
+        // Se a resposta não contiver um token, considere como falha de autenticação
+        setMensagem('Usuário ou senha incorretos');
       }
     } catch (error) {
+      // Se houver um erro na requisição, exiba uma mensagem de erro genérica
+      setMensagem('Erro ao realizar o login');
       console.log(error);
     }
   };
 
   return (
     <>
-      <div className={estiloLogin.login_container}>
-        <form className={estiloLogin.login_form}>
-          <h1 className="text-center mb-4">Sistema de Registro Eletrônico</h1>
-          <div className="d-flex flex-column align-items-center">
-            <img src={logo} className={`${estiloLogin.logo}`} alt="..." />
-          </div>
-          <p className="text-center mt-4">
+      <div>
+        <div className={estiloLogin.login_container}>
+          <h1 className="text-center mb-0">Sistema de Registro Eletrônico</h1>
+          <p className="text-center mt-0 fs-5">
             17º Pelotão de Comunicações de Selva
           </p>
-          <div className="mb-3">
-            <label htmlFor="exampleInputEmail1" className="form-label">
-              Usuário
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="usuario"
-              placeholder="Insira seu nome de usuário"
-              value={usuario}
-              onChange={(e) => setUsuario(e.target.value)}
-            />
+          <div className={estiloLogin.form}>
+
+            <div className={estiloLogin.logo_container}>
+              <div className="d-flex flex-column align-items-center">
+                <img src={logo} className={`${estiloLogin.logo}`} alt="..." />
+              </div>
+            </div>
+
+            <form className={estiloLogin.login_form}>
+              <div className="mb-3">
+                <label htmlFor="exampleInputEmail1" className="form-label">
+                  Usuário
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="usuario"
+                  placeholder="Insira seu nome de usuário"
+                  value={usuario}
+                  onChange={(e) => setUsuario(e.target.value)}
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="exampleInputPassword1" className="form-label">
+                  Senha
+                </label>
+                <input
+                  type="password"
+                  placeholder="Senha"
+                  className="form-control"
+                  value={senha}
+                  onChange={(e) => setSenha(e.target.value)}
+                />
+              </div>
+              <button
+                type="submit"
+                className="btn btn-success w-100"
+                onClick={handleLogin}
+              >
+                Entrar
+              </button>
+              {mensagem && (
+                <p
+                  className={`${estiloLogin.msg} text-center mt-2 ${mensagem === "Usuário autenticado!"
+                    ? "alert alert-success"
+                    : "alert alert-danger"
+                    }`}
+                >
+                  {mensagem}
+                </p>
+              )}
+
+            </form>
           </div>
-          <div className="mb-3">
-            <label htmlFor="exampleInputPassword1" className="form-label">
-              Password
-            </label>
-            <input
-              type="password"
-              placeholder="Senha"
-              className="form-control"
-              value={senha}
-              onChange={(e) => setSenha(e.target.value)}
-            />
-          </div>
-          <button
-            type="submit"
-            className="btn btn-success w-100"
-            onClick={handleLogin}
-          >
-            Entrar
-          </button>
-          {mensagem && (
-            <p
-              className={`${estiloLogin.msg} text-center mt-2 ${mensagem === "Usuário autenticado!"
-                ? "alert alert-success"
-                : "alert alert-danger"
-                }`}
-            >
-              {mensagem}
-            </p>
-          )}
-        </form>
+        </div>
       </div>
     </>
   );
