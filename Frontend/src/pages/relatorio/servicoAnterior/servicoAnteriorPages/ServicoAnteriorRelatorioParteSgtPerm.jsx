60d6884f3eef4eed80a95ea4da1056345202352a
Raphael Santiago <raphael.sant.emp@gmail.com>
2024-07-15 12:37:25 -0400

    Servico anterior Relatorio Parte do Sgt Permanencia ajustado para consulta ao BD

--- Frontend/src/pages/relatorio/servicoAnterior/servicoAnteriorPages/ServicoAnteriorRelatorioParteSgtPerm.jsx
+++ Frontend/src/pages/relatorio/servicoAnterior/servicoAnteriorPages/ServicoAnteriorRelatorioParteSgtPerm.jsx
@@ -1,59 +1,40 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
-import { capturaAno, capturaDia, capturaMes } from "../../../../components/util/capturaData.jsx";
 
-import ImpressaoFooter from "../../../../components/impressao/impressaoFooter";
+import ImpressaoFooter from "../../../../components/impressao/impressaoFooter.jsx";
 import estiloImpressao from "../../../../components/impressao/css/PrintPortrait.module.css";
 import "../../../../css/estiloTabela.css";
 
 import Navbar from "../../../../components/navbar";
-import { Imprimir } from "../../../../components/botao";
+import {
+    EditarRegistros,
+    Imprimir,
+} from "../../../../components/botao";
 import dbConfig from "../../../../components/util/dbConfig.jsx";
+import { formatDate } from "../../../../components/util/formatDateTime.jsx";
 
 export default function ServicoAnteriorRelatorioParteSgtPerm() {
-    let ontem = new Date().setHours(-1);
-    ontem = new Date(ontem); // o comando setHours devolve a data em milisegundos
-    const dataOntem = ontem.toLocaleDateString('pt-BR');
+    const selectedDate = localStorage.getItem('selectedDate');
 
-    // Estado para receber os dados gravados no BD
+    // Estado para armazenar os dados obtidos da API
     const [data, setData] = useState([]);
-    const [dataMil, setDataMil] = useState([]);
+    // Estado para indicar se os dados estão sendo carregados
+    const [loading, setLoading] = useState(true);
 
-    // Função para buscar dados da API e atualizar o estado 'data' - Parte Sgt Permanencia
+    // Função interna para buscar os dados da API e atualizar o estado 'data'
     const fetchData = async () => {
         try {
-            // Faz uma requisição para buscar dados da API em http://localhost:8081/relatorio_parte_sgt
-            const res = await fetch(`${dbConfig()}/servico_anterior_relatorio_parte_sgt`);
-
-            // Converte a resposta da requisição para o formato JSON
-            const fetchedData = await res.json();
-
-            // Atualiza o estado 'data' do componente com os dados obtidos da API
-            setData(fetchedData);
-        } catch (err) {
-            // Em caso de erro na requisição, exibe um alerta e imprime o erro no console
-            alert(err)
-            console.log(err);
-        }
-    };
-
-    // Função para buscar dados da API e atualizar o estado 'dataMil' - Roteiro da guarda
-    const fetchDataGuarnicao = async () => {
-        try {
-            // Faz uma requisição para buscar dados da API em http://localhost:8081/relatorio_roteiro_guarda
-            const res = await fetch(`${dbConfig()}/servico_anterior_relatorio_roteiro_guarda`);
-
-            // Converte a resposta da requisição para o formato JSON
-            const fetchedData = await res.json();
-
-            // Atualiza o estado 'data' do componente com os dados obtidos da API
-            setDataMil(fetchedData);
-        } catch (err) {
-            // Em caso de erro na requisição, exibe um alerta e imprime o erro no console
-            alert(err)
-            console.log(err);
+            const response = await axios.get(`${dbConfig()}/servico_anterior_configuracao_servico/${selectedDate}`);
+            setData(response.data);
+        } catch (error) {
+            console.error("Erro ao buscar os serviços:", error);
+        } finally {
+            setLoading(false); // Indica que o carregamento terminou
         }
     };
 
@@ -61,8 +42,27 @@ export default function ServicoAnteriorRelatorioParteSgtPerm() {
     useEffect(() => {
         // Chama a função fetchData para buscar dados da API e atualizar o estado 'data'
         fetchData();
-        fetchDataGuarnicao();
     }, []);
+
+    // Se os dados ainda estão sendo carregados, mostra uma mensagem de carregamento
+    if (loading) {
+        return <div>Carregando...</div>;
+    }
+
+    // Definindo datas e formatando-as
+    const dataAtual = new Date(data[0].servico_ref);
+    const dataPosterior = new Date(data[0].servico_ref);
+
+    const options = { day: 'numeric', month: 'long', year: 'numeric' };
+    dataPosterior.setDate(dataPosterior.getDate() + 1); // Obtém a data do dia Posterior
+
+    const dataPorExtenso = new Intl.DateTimeFormat('pt-BR', options).format(dataAtual);
+    const dataPosteriorPorExtenso = new Intl.DateTimeFormat('pt-BR', options).format(dataPosterior);
+
+
+    // Quando os dados estiverem disponíveis, faz o log
+    //console.log(data[0].servico_ref);
+
 
     return (
         <>
@@ -77,12 +77,15 @@ export default function ServicoAnteriorRelatorioParteSgtPerm() {
                             <Link to="/relatorio_servico_anterior">Serviço Anterior</Link>
                         </li>
                         <li className="breadcrumb-item active" aria-current="page">
+                            <Link to="/relatorio_servico_anterior/consulta_servico_anterior">Consulta ao dia {formatDate(selectedDate)}</Link>
+                        </li>
+                        <li className="breadcrumb-item active" aria-current="page">
                             Parte do Sgt Permanência
                         </li>
                     </ol>
                 </nav>
             </div>
-            <p className="text-center d-print-none">Parte do Sgt Permanência</p>
+            <p className="text-center d-print-none">Parte do Sgt Permanência do dia {formatDate(selectedDate)}</p>
 
             <div className={`container border border-dark bg-white ${estiloImpressao.container_local}`}>
                 <table className="table text-center table-bordered border-dark table-hover d-none d-print-block">
@@ -93,7 +96,7 @@ export default function ServicoAnteriorRelatorioParteSgtPerm() {
                                 Comando Militar da Amazônia – 12ª Região Militar <br />
                                 17ª Brigada de Infantaria de Selva <br />
                                 17º Pelotão de Comunicações de Selva <br />
-                                <span className="fw-light" style={{ fontSize: 15 + 'px' }}>Parte do Sgt Permanência, referente ao serviço do dia {capturaDia() - 1} para o dia {capturaDia()} de {capturaMes()} de {capturaAno()}</span>
+                                <span className="fw-light" style={{ fontSize: 15 + 'px' }}>Parte do Sgt Permanência, referente ao serviço do dia {dataPorExtenso} para o dia {dataPosteriorPorExtenso}</span>
                             </th>
                             <th scope="col" className="col-3">Enc Material</th>
                         </tr>
@@ -106,23 +109,19 @@ export default function ServicoAnteriorRelatorioParteSgtPerm() {
                                 <p className="my-1"><b>01 – Parada Diária:</b> {parteSgt.paradaDiaria}</p>
                                 <p className="my-1"><b>02 – Recebimento do Serviço:</b> {parteSgt.recebimentoServico}</p>
                                 <p className="my-1"><b>03 – Pessoal de Serviço:</b> {parteSgt.pessoalServico}</p>
-                                {dataMil.map((militares) => {
-                                    return (
-                                        <table className="table table-bordered border-dark table-hover table-local" key={militares.id}>
-                                            <tbody>
-                                                <tr>
-                                                    <td><b>Cmt da Gda: </b>{militares.sgtNomeGuerra}</td>
-                                                </tr>
-                                                <tr>
-                                                    <td><b>Cb da Gda: </b> {militares.cbNomeGuerra}</td>
-                                                </tr>
-                                                <tr>
-                                                    <td><b>Plantões: </b> {militares.sdPrimeiroHorNome}; {militares.sdSegundoHorNome} e {militares.sdTerceiroHorNome}.</td>
-                                                </tr>
-                                            </tbody>
-                                        </table>
-                                    );
-                                })}
+                                <table className="table table-bordered border-dark table-hover table-local" key={parteSgt.id}>
+                                    <tbody>
+                                        <tr>
+                                            <td><b>Cmt da Gda: </b>{parteSgt.sgtNomeGuerra}</td>
+                                        </tr>
+                                        <tr>
+                                            <td><b>Cb da Gda: </b> {parteSgt.cbNomeGuerra}</td>
+                                        </tr>
+                                        <tr>
+                                            <td><b>Plantões: </b> {parteSgt.sdPrimeiroHorNome}; {parteSgt.sdSegundoHorNome} e {parteSgt.sdTerceiroHorNome}.</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
                                 <p className="my-1"><b>04 - Energia Elétrica:</b></p>
                                 <table className="table text-center table-bordered border-dark table-hover table-local">
                                     <thead>
@@ -179,7 +178,6 @@ export default function ServicoAnteriorRelatorioParteSgtPerm() {
                     <ImpressaoFooter />
                 </div>
             </div>
-
         </>
     );
 }
