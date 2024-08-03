9c2dfb5498cf5c74e3dbdd40d149c44e8eb23c7d
Raphael Santiago <raphael.sant.emp@gmail.com>
2024-08-02 20:25:28 -0400

    Ajustado com o sweetalert2

--- Frontend/src/pages/pelotao/duranteExpediente/index.jsx
+++ Frontend/src/pages/pelotao/duranteExpediente/index.jsx
@@ -21,6 +21,8 @@ import clearForm from "../../../components/util/clearForm";
 import { formatDate, formatTime } from "../../../components/util/formatDateTime";
 import dbConfig from "../../../components/util/dbConfig";
 import { getLatestConfigServicoId } from "../../../components/configServico";
+import { toast } from "react-toastify";
+import Swal from "sweetalert2";
 
 export default function PelotaoDuranteExpediente() {
     // Estado para receber os dados gravados no BD
@@ -68,7 +70,8 @@ export default function PelotaoDuranteExpediente() {
             }
         } catch (error) {
             // Em caso de erro, exibe um alerta e retorna
-            alert('Erro ao obter a configuração do serviço:', error);
+            // alert('Erro ao obter a configuração do serviço:', error);
+            toast.error(error);
             return;
         }
 
@@ -112,20 +115,37 @@ export default function PelotaoDuranteExpediente() {
             const responseData = await response.json();
 
             if (responseData.status != 400) {
+                Swal.fire({
+                    position: "center",
+                    icon: "success",
+                    title: `${responseData.message}`,
+                    showConfirmButton: false,
+                    timer: 2000
+                });
+
                 // Limpa o formulário após a inserção
                 clearForm();
                 // Atualiza os dados na tela após a inserção 
                 // (supõe-se que fetchData() é uma função que busca os dados atualizados)
                 fetchData();
+            } else {
+                Swal.fire({
+                    position: "center",
+                    icon: "error",
+                    title: responseData.message,
+                    showConfirmButton: false,
+                    timer: 2000
+                });
             }
 
             // Exibe um alerta com a mensagem recebida do servidor após a inserção
-            alert(responseData.message);
+            //  alert(responseData.message);
 
 
         } catch (error) {
             // Em caso de erro na requisição, exibe um alerta
-            alert('Erro:', error);
+            toast.error(error);
+            // alert('Erro:', error);
         }
     };
 
@@ -170,8 +190,9 @@ export default function PelotaoDuranteExpediente() {
 
         } catch (error) {
             // Em caso de erro na requisição, exibe um alerta e imprime o erro no console
-            alert(error);
-            console.error("Erro ao buscar dados:", error);
+            toast.error(error);
+            // alert(error);
+            // console.error("Erro ao buscar dados:", error);
         }
     };
 
@@ -191,9 +212,17 @@ export default function PelotaoDuranteExpediente() {
             });
 
             // Exibe um alerta com a mensagem da resposta para informar o usuário sobre o resultado da operação
-            alert(response.data.message);
+            // alert(response.data.message);
 
             if (response.data.status != 400) {
+                Swal.fire({
+                    position: "center",
+                    icon: "success",
+                    title: `${response.data.message}`,
+                    showConfirmButton: false,
+                    timer: 2000
+                });
+
                 fetchData();
             }
 
@@ -202,7 +231,15 @@ export default function PelotaoDuranteExpediente() {
         } catch (error) {
             const mensagem = error.response.data.message;
             // Em caso de erro na requisição, exibe um alerta e imprime o erro no console
-            alert(mensagem);
+            //alert(mensagem);
+            // toast.error(mensagem);
+            Swal.fire({
+                position: "center",
+                icon: "error",
+                title: `${mensagem}`,
+                showConfirmButton: false,
+                timer: 2000
+            });
 
             // Lança o erro novamente para ser tratado por quem chamou essa função
             // throw error;
@@ -223,13 +260,46 @@ export default function PelotaoDuranteExpediente() {
             await fetchData();
 
             // Exibe um alerta da mensagem retornada após a exclusão (mensagem de sucesso ou erro)
-            alert(data.message);
+            //  alert(data.message);
         } catch (error) {
             // Em caso de erro na requisição, Exibe um alerta
-            alert('Erro:', error)
+            //  alert('Erro:', error)
+            toast.error(error);
         }
     };
 
+    // Função executada ao clicar no botao Deletar
+    const handleDeleteRegistro = (id, pg, nomeGuerra) => {
+        Swal.fire({
+            title: 'Tem certeza de que deseja excluir este registro?',
+            html: `PG: ${pg} <br> Nome: ${nomeGuerra}`,
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Sim, excluir!',
+            cancelButtonText: 'Cancelar',
+            customClass: {
+                confirmButton: 'btn btn-primary btn-lg',
+                cancelButton: 'btn btn-secondary btn-lg'
+            }
+        }).then((result) => {
+            if (result.isConfirmed) {
+                deleteRegistro(id);
+                Swal.fire({
+                    title: 'Excluído!',
+                    text: 'O registro foi excluído com sucesso.',
+                    icon: 'success',
+                    customClass: {
+                        title: 'success-title',
+                        popup: 'success-popup',
+                        confirmButton: 'btn btn-primary btn-lg',
+                        content: 'success-content'
+                    }
+                });
+            }
+        });
+    };
+
+    /*
     // Função executada ao clicar no botao Deletar
     const handleDeleteRegistro = (id, pg, nomeGuerra) => {
         // Exibe um diálogo de confirmação ao usuário, mostrando os detalhes do registro que será excluído
@@ -241,7 +311,7 @@ export default function PelotaoDuranteExpediente() {
             // Chama a função de exclusão se o usuário confirmar
             deleteRegistro(id);
         }
-    };
+    };*/
 
     return (
         <>
